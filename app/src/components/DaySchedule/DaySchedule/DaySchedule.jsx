c48bd8dd483cc3db7efcaca4059fff0009625c6e
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2024-06-10 11:41:59 -0400

    add hook useLatestLessons, Update DaySchedule

--- app/src/components/DaySchedule/DaySchedule/DaySchedule.jsx
+++ app/src/components/DaySchedule/DaySchedule/DaySchedule.jsx
@@ -3,7 +3,7 @@ import useLatestLessons from "../../../hooks/useLatestLessons";
 
 export default function DaySchedule({ children }) {
   const { latestLessons } = useLatestLessons();
-  console.log(latestLessons); // For debugging
+  // console.log(latestLessons); // For debugging
 
   return <section className={style.daySchedule}>{children}</section>;
 }
