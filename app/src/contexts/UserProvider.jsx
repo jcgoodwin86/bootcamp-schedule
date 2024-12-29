af1601a79579f152ae939d8e406ce0b9116cc79f
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2024-12-29 16:26:13 -0500

    update import statement for useSaveCompletedState

--- app/src/contexts/UserProvider.jsx
+++ app/src/contexts/UserProvider.jsx
@@ -2,14 +2,14 @@
 // It uses the useLoadStateFromDB hook to load the user data from the database
 // and the useSaveCompletedState hook to save the completed state of the user data to the database.
 import useLoadStateFromDB from "../hooks/db/useLoadStateFromDB";
-import useSaveCompletedState from "../hooks/db/useSaveCompletedState"; // assuming createDB contains the debounced hook
+import { useSaveCompletedState } from "../hooks/db/useSaveCompletedState"; // assuming createDB contains the debounced hook
 import { UserContext } from "./UserContext";
 
-export const UserProvider = ({ children }) => {
+export function UserProvider({ children }) {
   const { userData, setUserData } = useLoadStateFromDB();
   useSaveCompletedState(userData);
 
   return (
     <UserContext value={{ userData, setUserData }}>{children}</UserContext>
   );
-};
+}
